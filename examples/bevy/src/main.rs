2802da74b4c26d5da57e1633373ff67ccce36d06
agabani <agabani@users.noreply.github.com>
2023-11-27 21:51:02 +0000

    feat: examples bevy

--- examples/bevy/src/main.rs
+++ examples/bevy/src/main.rs
@@ -1,16 +1,25 @@
-use std::{sync::mpsc, thread, time::Duration};
-
-use bevy::utils::Uuid;
-
 mod artificial_intelligence;
 mod authority;
-mod client;
 mod identity;
 mod network;
 mod network_client;
 mod network_server;
 mod player;
-mod server;
+
+use std::{
+    sync::{mpsc, Mutex},
+    thread,
+    time::Duration,
+};
+
+use artificial_intelligence::ArtificialIntelligencePlugin;
+use authority::AuthorityPlugin;
+use bevy::{prelude::*, utils::Uuid};
+use identity::{Identifiers, Principal};
+use network::{ConnectionsRx, ConnectionsTx, Handshake};
+use network_client::NetworkClientPlugin;
+use network_server::NetworkServerPlugin;
+use player::PlayerPlugin;
 
 fn main() {
     let (connections_tx, connections_rx) = mpsc::channel();
@@ -19,7 +28,7 @@ fn main() {
         thread::spawn({
             let connections_rx = connections_rx;
             move || {
-                server::run(
+                server(
                     connections_rx,
                     authorization::Principal {
                         id: Uuid::new_v4().to_string(),
@@ -32,7 +41,7 @@ fn main() {
         thread::spawn({
             let connections_tx = connections_tx.clone();
             move || {
-                client::run(
+                client(
                     connections_tx,
                     authorization::Principal {
                         id: Uuid::new_v4().to_string(),
@@ -45,7 +54,7 @@ fn main() {
         thread::spawn({
             let connections_tx = connections_tx.clone();
             move || {
-                client::run(
+                client(
                     connections_tx,
                     authorization::Principal {
                         id: Uuid::new_v4().to_string(),
@@ -59,7 +68,7 @@ fn main() {
             let connections_tx = connections_tx.clone();
             thread::sleep(Duration::from_secs(5));
             move || {
-                client::run(
+                client(
                     connections_tx,
                     authorization::Principal {
                         id: Uuid::new_v4().to_string(),
@@ -74,4 +83,56 @@ fn main() {
     for thread in threads {
         thread.join().expect("thread terminated unexpectedly");
     }
+}
+
+pub fn client(connections_tx: mpsc::Sender<Handshake>, principal: authorization::Principal) {
+    let mut app = App::new();
+
+    app.add_plugins((
+        MinimalPlugins,
+        bevy::log::LogPlugin {
+            filter: "bevy_app=info,bevy_ecs=info,wgpu=warn".to_string(),
+            level: bevy::log::Level::TRACE,
+        },
+    ));
+
+    if principal.noun == "artificial_intelligence" {
+        app.add_plugins(ArtificialIntelligencePlugin);
+    }
+
+    if principal.noun == "authority" {
+        app.add_plugins(AuthorityPlugin);
+    }
+
+    app.add_plugins(NetworkClientPlugin)
+        .insert_resource(ConnectionsTx(connections_tx))
+        .insert_resource(Principal(principal));
+
+    app.insert_resource(Identifiers(Default::default()));
+
+    app.add_plugins(PlayerPlugin);
+
+    app.run();
+}
+
+pub fn server(connections_rx: mpsc::Receiver<Handshake>, principal: authorization::Principal) {
+    let mut app = App::new();
+
+    app.add_plugins((
+        MinimalPlugins,
+        bevy::log::LogPlugin {
+            filter: "bevy_app=info,bevy_ecs=info,wgpu=warn".to_string(),
+            level: bevy::log::Level::TRACE,
+        },
+    ));
+
+    app.add_plugins(NetworkServerPlugin)
+        .insert_resource(ConnectionsRx(Mutex::new(connections_rx)))
+        .insert_resource(Principal(principal));
+
+    app.insert_resource(Identifiers(Default::default()));
+
+    app.add_plugins(PlayerPlugin);
+
+    app.run();
 }
